19f17fb9cd80b1de3547abd4e0f3b4a153a7fb39
SSPENELOPE <typoepping@gmail.com>
2022-08-03 10:42:11 -0400

    fixed load text areas, removed background color change for further troubleshooting

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,36 @@
 /*     Variables     */
 var saveBtn9am = $('#saveBtn-9am');
 var textArea9am = $('#textarea-9am');
-var currentTime = moment().format('MMM Do, YYYY, HH:mm:ss');
+
 
 /*    Functions    */
 
 // Timer update function
 var update = function () {
+    var currentTime = moment().format('MMM Do, YYYY, HH:mm:ss');
     $('#currentDay').text(currentTime);
 };
 
 // Load Textarea's function
 function loadTextAreas() {
-    
-    // Not working, currently troubleshooting 
-    var stored9am = JSON.parse(localStorage.getItem('text'));
-    if (stored9am !== null) {
-       textArea9am = stored9am;
-    };
+    // Get Textarea from storage
+    var stored9am = JSON.parse(localStorage.getItem('textNine'));
+
+    // Display Textarea on page
+    textArea9am.val(stored9am);
 };
 
-function backgroundColorUpdate() {
-    if (currentTime < 10) {
-        textArea9am.addClass('present');
-    } else {
-        textArea9am.addClass('past')
-    };
-};
+
 
 /*     Click Events     */
 saveBtn9am.on('click', function() {
-    var text = textArea9am.val();
-    localStorage.setItem('text', JSON.stringify(text));
-    console.log(text);
+    var textNine = textArea9am.val();
+    localStorage.setItem('textNine', JSON.stringify(textNine));
+    console.log(textNine);
 });
 
 
 
-backgroundColorUpdate();
 loadTextAreas();
 setInterval(update, 1000);
 
