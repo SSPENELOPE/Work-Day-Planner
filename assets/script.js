984e6be8c1b4fc0ed21ef4876689233bd8067faa
SSPENELOPE <typoepping@gmail.com>
2022-08-03 11:57:08 -0400

    Finsihed loadTextArea function, added all the save buttons until they can be condensed

--- assets/script.js
+++ assets/script.js
@@ -35,9 +35,25 @@ function loadTextAreas() {
 
     // Get Textarea from storage
     var stored9am = JSON.parse(localStorage.getItem('textNine'));
+    var stored10am = JSON.parse(localStorage.getItem('textTen'));
+    var stored11am = JSON.parse(localStorage.getItem('textEleven'));
+    var stored12am = JSON.parse(localStorage.getItem('textTwelve'));
+    var stored1pm = JSON.parse(localStorage.getItem('textOne'));
+    var stored2pm = JSON.parse(localStorage.getItem('textTwo'));
+    var stored3pm = JSON.parse(localStorage.getItem('textThree'));
+    var stored4pm = JSON.parse(localStorage.getItem('textFour'));
+    var stored5pm = JSON.parse(localStorage.getItem('textFive'));
 
     // Display Textarea on page
     textArea9am.val(stored9am);
+    textArea10am.val(stored10am);
+    textArea11am.val(stored11am);
+    textArea12am.val(stored12am);
+    textArea1pm.val(stored1pm);
+    textArea2pm.val(stored2pm);
+    textArea3pm.val(stored3pm);
+    textArea4pm.val(stored4pm);
+    textArea5pm.val(stored5pm);
 };
 
 function timeCheck() {
@@ -58,9 +74,59 @@ saveBtn9am.on('click', function() {
     console.log(textNine);
 });
 
-$('button').each(function() {
-
+saveBtn10am.on('click', function() {
+    var textTen = textArea10am.val();
+    localStorage.setItem('textTen', JSON.stringify(textTen));
+    console.log(textTen);
 });
+
+saveBtn11am.on('click', function() {
+    var textEleven = textArea11am.val();
+    localStorage.setItem('textEleven', JSON.stringify(textEleven));
+    console.log(textEleven);
+});
+
+saveBtn12am.on('click', function() {
+    var textTwelve = textArea12am.val();
+    localStorage.setItem('textTwelve', JSON.stringify(textTwelve));
+    console.log(textTwelve);
+});
+
+saveBtn1pm.on('click', function() {
+    var textOne = textArea1pm.val();
+    localStorage.setItem('textOne', JSON.stringify(textOne));
+    console.log(textOne);
+});
+
+saveBtn2pm.on('click', function() {
+    var textTwo = textArea2pm.val();
+    localStorage.setItem('textTwo', JSON.stringify(textTwo));
+    console.log(textTwo);
+});
+
+saveBtn3pm.on('click', function() {
+    var textThree = textArea3pm.val();
+    localStorage.setItem('textThree', JSON.stringify(textThree));
+    console.log(textThree);
+});
+
+saveBtn4pm.on('click', function() {
+    var textFour = textArea4pm.val();
+    localStorage.setItem('textFour', JSON.stringify(textFour));
+    console.log(textFour);
+});
+
+saveBtn5pm.on('click', function() {
+    var textFive = textArea5pm.val();
+    localStorage.setItem('textFive', JSON.stringify(textFive));
+    console.log(textFive);
+});
+
+
+// Function for all buttons (wip)
+/*$('button').each(function() {
+
+});*/
 
 timeCheck();
 loadTextAreas();
